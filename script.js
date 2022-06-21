b3566d632afe9f7f5538cc6d8a135768985a2e5c
Julian2206 <julian.pcc991@gmail.com>
2022-06-21 14:50:43 +0200

    js(added-some-notes)

--- script.js
+++ script.js
@@ -5,15 +5,22 @@
     con un ciclo while.
  */
 
+// build an array with "obj";
 const shoppingList = ["obj1", "obj2", "obj3", "obj4", "obj5", "obj6"];
 
+// get html element by ID or querySelector/querySelectorAll;
 const shoppingCart = document.getElementById("cart");
 
+// set the counter to 0;
 let index = 0;
 
 while (index < shoppingList.length) {
+  // create li elements;
   const cartElement = document.createElement("li");
+  //set cartElement to inner html string
   cartElement.innerHTML = shoppingList[index];
+  // append cartElement to html
   shoppingCart.append(cartElement);
+  // counter index = index +1;
   index++;
 }
